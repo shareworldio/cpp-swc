901efa27f1f013aa9b5afbb5c89a64fc76d94cbe
ancjf <ancjf@163.com>
2018-04-27 16:49:48 +0800

    改进默认配置参数

--- libethashseal/genesis/quark.cpp
+++ libethashseal/genesis/quark.cpp
@@ -5,16 +5,16 @@ R"E(
 {
 	"sealEngine": "QPOS",
 	"params": {
-		"syncRequest": "0x1",
+		"syncRequest": "0x01",
 		"accountStartNonce": "0x00",
-		"homesteadForkBlock": "0x118c30",
-		"daoHardforkBlock": "0x1d4c00",
-		"EIP150ForkBlock": "0x259518",
-		"EIP158ForkBlock": "0x28d138",
-		"byzantiumForkBlock": "0x42ae50",
-		"constantinopleForkBlock": "0x500000",
-		"networkID" : "0x01",
-		"chainID": "0x01",
+		"homesteadForkBlock": "0x00",
+		"daoHardforkBlock": "0x00",
+		"EIP150ForkBlock": "0x00",
+		"EIP158ForkBlock": "0x00",
+		"byzantiumForkBlock": "0x00",
+		"constantinopleForkBlock": "0x00",
+		"networkID" : "0xfe",
+		"chainID": "0xfe",
 		"maximumExtraDataSize": "0x20",
 		"tieBreakingGas": false,
 		"minGasLimit": "0x1388",
@@ -26,14 +26,14 @@ R"E(
 		"blockReward": "0x4563918244F40000"
 	},
 	"genesis": {
-		"nonce": "0x000000000000001",
-		"difficulty": "0x40",
+		"nonce": "0x0000000000000042",
+		"difficulty": "0xfe",
 		"mixHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
 		"author": "0x0000000000000000000000000000000000000000",
 		"timestamp": "0x00",
 		"parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
 		"extraData": "0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa",
-		"gasLimit": "0x13880000000000000000"
+		"gasLimit": "0x1000000"
 	},
 	"accounts": {
 		"0000000000000000000000000000000000000001": { "precompiled": { "name": "ecrecover", "linear": { "base": 3000, "word": 0 } } },
@@ -50,5 +50,4 @@ R"E(
 		"0xbd30Bb846A187DEF21ACaC7f4A1317c97fB98197": {"balance": "1337000000"}
 	}
 }
-
 )E";
